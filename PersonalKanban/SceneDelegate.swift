777768e6018d8564339f51a1c4942da1f5101683
Hunter Buxton <hunter.buxton@protonmail.com>
2020-10-17 21:37:15 -0600

    removed Main.storyboard and set up SceneDelegate to initialize the window programatically

--- PersonalKanban/SceneDelegate.swift
+++ PersonalKanban/SceneDelegate.swift
@@ -16,7 +16,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // https://learnappmaking.com/scene-delegate-app-delegate-xcode-11-ios-13/
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let rootVC = MainViewController()
+            let navigation = UINavigationController(rootViewController: rootVC)
+            window.rootViewController = navigation
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
