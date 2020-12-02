772e33e8a60360b3fa686f70679756944fefeff8
Hunter Buxton <hunter.buxton@protonmail.com>
2020-12-02 12:22:19 -0700

    unit tests for EpicInputValidationManager

--- InputValidatorTests/InputValidatorTests.swift
+++ InputValidatorTests/InputValidatorTests.swift
@@ -1,46 +1,46 @@
-////
-////  InputValidatorTests.swift
-////  InputValidatorTests
-////
-////  Created by Hunter Buxton on 10/26/20.
-////
 //
-//import XCTest
-//@testable import PersonalKanban
+//  InputValidatorTests.swift
+//  InputValidatorTests
 //
-//class InputValidatorTests: XCTestCase {
+//  Created by Hunter Buxton on 10/26/20.
 //
-//    var sut: InputValidationManager!
-//
-//    override func setUpWithError() throws {
-//        super.setUp()
-//        sut = InputValidationManager()
-//    }
-//
-//    override func tearDownWithError() throws {
-//        sut = nil
-//        super.tearDown()
-//    }
-//
-//    func testExample() throws {
-//        let titleTracker = sut.inputTrackers[.title]
-//        XCTAssertTrue(titleTracker != nil, "inputvalidationmanager had a nil value for the 'title' tracker")
-//
-//        sut.inputUpdate(nil, from: .title)
-//        XCTAssertFalse(titleTracker!.approved, "inputvalidationmanager accepted nil for the title input")
-//
-//        sut.inputUpdate("", from: .title)
-//        XCTAssertFalse(titleTracker!.approved, "inputvalidationmanager accepted an empty string for the title input")
-//
-//        sut.inputUpdate("    ", from: .title)
-//        XCTAssertFalse(titleTracker!.approved, "inputvalidationmanager accepted an all-whitespace string for the title input")
-//    }
-//
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-//
-//}
+
+import XCTest
+@testable import PersonalKanban
+
+class InputValidatorTests: XCTestCase {
+
+    var sut: EpicInputValidationManager!
+
+    override func setUpWithError() throws {
+        super.setUp()
+        sut = EpicInputValidationManager()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        super.tearDown()
+    }
+
+    func testExample() throws {
+        let titleTracker = sut.inputTrackers[.title]
+        XCTAssertTrue(titleTracker != nil, "inputvalidationmanager had a nil value for the 'title' tracker")
+
+        sut.inputUpdate(nil, from: .title)
+        XCTAssertFalse(titleTracker!.approved, "inputvalidationmanager accepted nil for the title input")
+
+        sut.inputUpdate("", from: .title)
+        XCTAssertFalse(titleTracker!.approved, "inputvalidationmanager accepted an empty string for the title input")
+
+        sut.inputUpdate("    ", from: .title)
+        XCTAssertFalse(titleTracker!.approved, "inputvalidationmanager accepted an all-whitespace string for the title input")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
